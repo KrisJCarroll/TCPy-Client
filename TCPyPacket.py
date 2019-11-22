4e0824156bea6803ac881e7855ef428603914d06
Kristopher Carroll <kjcarroll@alaska.edu>
2019-11-22 11:13:26 -0900

    added checksum validation and packet unpacking

--- TCPyPacket.py
+++ TCPyPacket.py
@@ -43,6 +43,19 @@ class TCPyPacket:
         checksum = cs_calc.finalbytes()
         packet.overwrite(checksum, 128)
 
+    def valid_checksum(bytes_packet, source_address, dest_address):
+        bin_packet = BitStream(bytes=bytes_packet)
+        length = len(bin_packet) % 8
+        checksum = bin_packet[128:144].bytes
+        pseudo_header = TCPyPacket.create_pseudo_header(source_address, dest_address, length)
+        bin_packet.overwrite(b'\x00\x00', 128)
+        cs_calc = cs.Checksum16()
+        cs_calc.process(bin_packet.tobytes())
+        cs_calc.process(pseudo_header)
+        if checksum != cs_calc.finalbytes():
+            return False
+        return True
+
     def create_pseudo_header(source_address, dest_address, length):
         pseudo_header  = bytearray()
         pseudo_header += bytearray(socket.inet_aton(source_address))
@@ -51,6 +64,23 @@ class TCPyPacket:
         pseudo_header += bytearray(TCPyPacket.TCP_PTCL) 
         pseudo_header += length.to_bytes(2, byteorder="big")
         return pseudo_header
+
+    def unpack_packet(bytes_packet):
+        packet_binary = BitStream(bytes=bytes_packet)
+        if not TCPyPacket.valid_checksum(bytes_packet):
+            return None
+        packet_dict = {
+            'S_PORT' : packet_binary[0:16].int,
+            'D_PORT' : packet_binary[16:32].int,
+            'SEQ_NUM': packet_binary[32:64].int,
+            'ACK_NUM': packet_binary[64:96].int,
+            'OFFSET' : packet_binary[96:100].int,
+            'ACK'    : packet_binary[107],
+            'SYN'    : packet_binary[110],
+            'FIN'    : packet_binary[111],
+            'WINDOW' : packet_binary[112:128],
+        }
+        return packet_dict
 
     # returns syn/ack'ed seq number and ack number if SYN and ACK set, else returns (False, False)
     def check_synack(packet_bytes):
@@ -73,7 +103,6 @@ class TCPyPacket:
     def package_packet(source_address, dest_address, source_port, dest_port, seq_num, ack_num, 
                        offset = 5, ack = False, syn = False, fin = False, 
                        window = 0, data = None):
-
         packet_dict = {'source_port': source_port, 'dest_port': dest_port,
                        'seq_num': seq_num,
                        'ack_num': ack_num,
@@ -87,7 +116,6 @@ class TCPyPacket:
         pack_format += ' uint:4=offset, uint:6=reserved, bool=U, bool=A, bool=P, bool=R, bool=S, bool=F, uint:16=window,'
         pack_format += ' bytes:2=checksum, uint:16=urgent,'
         header_binary = bs.pack(pack_format, **packet_dict)
-        print(len(header_binary))
         # combine the header and the data binary
         if data:
             data_binary = BitStream(bytes=data)
